d7c045b48c1c0854e4e3260a58b0429392dae1f1
Tiranoe <jungwoo9543@gmail.com>
2022-10-31 17:10:33 -0400

    fixed errors for portfolio

--- db/create_projects.sql
+++ db/create_projects.sql
@@ -1,16 +1,15 @@
-INSERT INTO main_app_project(name, img, tool, description, github, live)
+INSERT INTO main_app_project(name, img, tool, description, github, live, created_at)
     VALUES
-        ('adventureTimes', 'https://github.com/Tiranoe/adventureTime/blob/main/assets/adventuretimegif.gif?raw=true', 'Python, Django', 'Community Applications for travelers to share their unique experiences', 'https://github.com/Tiranoe/adventureTime', 'https://adventuretimes.herokuapp.com/')
+        ('adventureTimes', 'https://github.com/Tiranoe/adventureTime/blob/main/assets/adventuretimegif.gif?raw=true', 'Python, Django', 'Community Applications for travelers to share their unique experiences', 'https://github.com/Tiranoe/adventureTime', 'https://adventuretimes.herokuapp.com/', '2022-10-31 17:00:00');
 
-INSERT INTO main_app_project(name, img, tool, description, github, live)
+INSERT INTO main_app_project(name, img, tool, description, github, live, created_at)
     VALUES
-        ('Instagram Cloning', 'https://github.com/Tiranoe/Instagram-Cloning/blob/main/assets/instaclone.gif?raw=true', 'JavaScript, EJS,  Node, Express, MongoDB', 'Instagram is a full-stack web application in which users are able to post images for the world to see, along with browsing others images, liking them, and commenting on them. Our team of three developers implemented a version of Instagram that allows users to do the same things that they can on the real version in a one-week sprint.', 'https://github.com/Tiranoe/Instagram-Cloning', 'https://instagram-clone-713.herokuapp.com/')
+        ('Instagram Cloning', 'https://github.com/Tiranoe/Instagram-Cloning/blob/main/assets/instaclone.gif?raw=true', 'JavaScript, EJS,  Node, Express, MongoDB', 'Instagram is a full-stack web application in which users are able to post images for the world to see, along with browsing others images, liking them, and commenting on them. Our team of three developers implemented a version of Instagram that allows users to do the same things that they can on the real version in a one-week sprint.', 'https://github.com/Tiranoe/Instagram-Cloning', 'https://instagram-clone-713.herokuapp.com/', '2022-10-31 17:00:00');
 
-INSERT INTO main_app_project(name, img, tool, description, github, live)
+INSERT INTO main_app_project(name, img, tool, description, github, live, created_at)
     VALUES
-        ('Tamagotchi', 'https://github.com/Tiranoe/TAMA/blob/main/assets/ezgif-5-9cb8e12fd0.gif?raw=true', 'JavaScript, HTML, CSS', 'A clone of popular game called: Tamagotchi.', 'https://github.com/Tiranoe/TAMA', 'https://tiranoe.github.io/TAMA/#')
+        ('Tamagotchi', 'https://github.com/Tiranoe/TAMA/blob/main/assets/ezgif-5-9cb8e12fd0.gif?raw=true', 'JavaScript, HTML, CSS', 'A clone of popular game called: Tamagotchi.', 'https://github.com/Tiranoe/TAMA', 'https://tiranoe.github.io/TAMA/#', '2022-10-31 17:00:00');
 
-INSERT INTO main_app_project(name, img, tool, description, github, live)
+INSERT INTO main_app_project(name, img, tool, description, github, live, created_at)
     VALUES
-        ('Travelizer', 'https://github.com/Tiranoe/Travel-Cities/blob/main/assets/travelizer%20gif.gif?raw=true', 'React, API, JavaScript', 'My app is called Travelizer. This app will show list of countries for the user to determine which country to choose for their vacation time. User will be able to see all the list of countries in the database. When user clicks each country name, it will display countrys name, capital city, currency, flag, phone code, and other information regarding to that country.', 'https://github.com/Tiranoe/Travel-Cities', 'https://golden-donut-7a98ce.netlify.app/')
-    
+        ('Travelizer', 'https://github.com/Tiranoe/Travel-Cities/blob/main/assets/travelizer%20gif.gif?raw=true', 'React, API, JavaScript', 'My app is called Travelizer. This app will show list of countries for the user to determine which country to choose for their vacation time. User will be able to see all the list of countries in the database. When user clicks each country name, it will display countrys name, capital city, currency, flag, phone code, and other information regarding to that country.', 'https://github.com/Tiranoe/Travel-Cities', 'https://golden-donut-7a98ce.netlify.app/', '2022-10-31 17:00:00'); 
