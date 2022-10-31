f360e8adb52569770dde934ea14423b517b5e30e
Tiranoe <jungwoo9543@gmail.com>
2022-10-31 17:31:03 -0400

    project detail page is now added

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="aboutme"),
     path('projects/', views.ProjectList.as_view(), name="project_list"),
+    path('projects/<int:pk>/', views.ProjectDetail.as_view(), name="project_detail"),
     path('contact/', views.Contact.as_view(), name="contact me"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View 
 from django.views.generic.base import TemplateView
 from .models import Project
+from django.views.generic import DetailView
+
 
 class Home(TemplateView):
     template_name = "home.html"
@@ -16,6 +18,10 @@ class ProjectList(TemplateView):
         context = super().get_context_data(**kwargs)
         context["projects"] = Project.objects.all()
         return context
+
+class ProjectDetail(DetailView):
+    model = Project
+    template_name = "project_detail.html"
 
 class Contact(TemplateView):
     template_name = "contact.html"
